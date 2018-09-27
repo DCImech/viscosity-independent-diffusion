e2a11d7efa5ed39f89023793d3e4b0c626900322
dyanni3 <Davidyanni1406@gmail.com>
2018-09-27 14:25:48 -0400

    alphas
    added simulation for alphas, switched time scale

--- visc_ind_diff.py
+++ visc_ind_diff.py
@@ -15,7 +15,7 @@ from scipy import io as sio
 #%% define constants and set-up
 E = 1
 eta = 1
-T = 10000 #total num time steps
+T = 20000 #total num time steps
 lambda_act = 1e-2 #rate of birth/death events
 N = 10000 #length of biofilm
 sigma_0 = 10
@@ -48,7 +48,7 @@ plt.plot(t,np.average(epsilon**2,axis=1))
 etas = [.1,1,10,100,1000]
 plt.figure(figsize=(8,8))
 for i,eta in enumerate(etas):
-	M = 1000 #num trials
+	M = 100 #num trials
 	times = np.zeros((M,T))
 	eps2 = np.zeros((M,T))
 	for m in range(M):
@@ -70,3 +70,76 @@ for i,eta in enumerate(etas):
 	sio.savemat("diff_eta%d"%i,{"eps2":np.average(eps2,axis=0)})
 	sio.savemat("diff_eta_times%d"%i,{"times":np.average(times,axis=0)})
 plt.legend()
+
+#%% run many trials
+etas = np.array([.1,100,1000])
+lambda_acts = 1e-6*((E/etas)**1) 
+plt.figure(figsize=(8,8))
+plt.xlabel("Time",fontsize=16)
+plt.ylabel(r"$\langle\Delta\,x^2\rangle$",fontsize=16)
+for i,eta in enumerate(etas):
+	M = 500 #num trials
+	times = np.zeros((M,T))
+	eps2 = np.zeros((M,T))
+	for m in range(M):
+		if m%10==0:
+			print(m)
+		j = np.random.randint(0,N,size=T)
+		s = np.random.choice([-1,1], size=T)
+		dt = np.random.exponential(scale = 1/lambda_acts[i], size = T)
+		t = np.cumsum(dt)
+		sigma = np.zeros(shape=(T,N))
+		epsilon = np.zeros(shape=(T,N))
+		for k in range(T):
+			sigma[k,:j[k]] = sigma[k-1,:j[k]] - sigma_0*s[k]
+			sigma[k,j[k]:] = sigma[k-1,j[k]:] + sigma_0*s[k]
+			epsilon[k,:] = (1 / (1 + dt[k]*E/eta)) * (epsilon[k-1,:] + (dt[k]/eta)*sigma[k,:])
+		eps2[m,:] = np.average(epsilon**2,axis=1)
+		times[m,:] = t
+	plt.loglog(np.average(times,axis=0),np.average(eps2,axis=0),marker='o',label=r"$\eta=$ %s"%str(eta))
+	sio.savemat("diff_eta_long_passive_MSD%d"%i,{"eps2":np.average(eps2,axis=0)})
+	sio.savemat("diff_eta_long_passive_times%d"%i,{"times":np.average(times,axis=0)})
+plt.legend()
+#%% plotting D vs \eta
+
+plt.figure(figsize=(8,8))
+plt.xlabel(r"$\eta$",fontsize=24)
+plt.ylabel("D",fontsize=24)
+for i in range(6):
+    if(i==0 or i==5):
+        plt.loglog(fake_etas,fake_Ds[i],marker='o',label=r"$\alpha=$ %s"%str(alphas[i]),lw=6,markersize=12)
+    else:
+        plt.loglog(fake_etas,fake_Ds[i],marker='o',label=r"$\alpha=$ %s"%str(alphas[i]))
+plt.legend(fontsize=16)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+#%%
+etas = np.array([.1,1,10,100,1000])
+for j,alpha in enumerate(alphas):
+	lambda_acts = 1e-2  *((E/etas)**alpha) 
+	plt.figure(figsize=(8,8))
+	plt.xlabel("Time",fontsize=16)
+	plt.ylabel(r"$\langle\Delta\,x^2\rangle$",fontsize=16)
+	for i,eta in enumerate(etas):
+		M = 1000 #num trials
+		times = np.zeros((M,T))
+		eps2 = np.zeros((M,T))
+		for m in range(M):
+			if m%10==0:
+				print(m)
+			j = np.random.randint(0,N,size=T)
+			s = np.random.choice([-1,1], size=T)
+			dt = np.random.exponential(scale = 1/lambda_acts[i], size = T)
+			t = np.cumsum(dt)
+			sigma = np.zeros(shape=(T,N))
+			epsilon = np.zeros(shape=(T,N))
+			for k in range(T):
+				sigma[k,:j[k]] = sigma[k-1,:j[k]] - sigma_0*s[k]
+				sigma[k,j[k]:] = sigma[k-1,j[k]:] + sigma_0*s[k]
+				epsilon[k,:] = (1 / (1 + dt[k]*E/eta)) * (epsilon[k-1,:] + (dt[k]/eta)*sigma[k,:])
+			eps2[m,:] = np.average(epsilon**2,axis=1)
+			times[m,:] = t
+		plt.loglog(np.average(times,axis=0),np.average(eps2,axis=0),marker='o',label=r"$\eta=$ %s"%str(eta))
+		sio.savemat("diff_eta_MSD%s_alpha%s"%(str(eta),str(alpha)),{"eps2":np.average(eps2,axis=0)})
+		sio.savemat("diff_eta_times%s_alpha%s"%(str(eta),str(alpha)),{"times":np.average(times,axis=0)})
+	plt.legend()
